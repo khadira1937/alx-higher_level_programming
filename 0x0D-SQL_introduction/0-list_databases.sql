15317cec2e1234f8b4e0fcd9aac79d8540d9ac4a
khadira1937 <okhadira@gmail.com>
2023-08-15 12:41:16 -0700

    sql

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,2 +1,2 @@
---script that lists all databases of MySQL server.
+-- Lists all databases of my MySQL server.
 SHOW DATABASES;
--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
---script that creates the database hbtn_0c_00.
-CREATE DATABASE IF NOT EXISTs `hbtn_0c_0`
+-- Creates the database hbtn_0c_0 in my MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
