9429511c80fb7e1b858c41cfe982a9c493d82a44
khadira1937 <okhadira@gmail.com>
2023-08-16 18:09:04 -0700

    more sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+--script that creates the MySQL server user user_0d_1
+--user_0d_1 should have all privileges in  MySQL server
+--password should be set to user_0d_1_pwd
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO user_0d_1@localhost
