a5cb0b68d69e8435efef8d3d351088ebaea9cfce
khadira1937 <okhadira@.com>
2023-09-11 16:09:09 -0700

    warm-up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const table = ['C is fun', 'nPython is cool', 'nJavaScript is amazing'];
-for (let i = 0; i < table.length; i++) {
+for (let i = 0; i < 3 ; i++) {
   console.log(table[i]);
 }
